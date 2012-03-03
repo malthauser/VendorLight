5d95dd7ec694211d9333ea0642f781eb4d1332c7
Wahaj Ali <wahaj.ali@7vals.com>
2012-03-03 15:22:22 +0500

    Set up intial user model and set up devise

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+
+  validates_presence_of :first_name, :last_name
+
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+
+  def full_name
+    "#{first_name} #{last_name}".strip 
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VendorLight::Application.config.secret_token = 'f14a51c5dda33bd103d41413ae7f3d102fd8a83cb810a7cf12696c14347aab1290061467c6325677480cbdc3d312444ee6fc932cc15edaf122c74d415e84f298'
