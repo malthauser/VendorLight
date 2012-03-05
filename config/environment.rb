65d007e3591bcc37027763f3323467c05b60193a
Shirjeel Alam <shirjeel20@gmail.com>
2012-03-05 23:04:04 +0500

    Setup action mailer for production

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+config.action_mailer.default_url_options = { :host => 'localhost' }
+
 # Initialize the rails application
 VendorLight::Application.initialize!
