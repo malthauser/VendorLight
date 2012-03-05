778fdc1cc6cccaa04d4cb1e64b22710fe562f3dd
Shirjeel Alam <shirjeel20@gmail.com>
2012-03-05 23:06:05 +0500

    Small fix

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-config.action_mailer.default_url_options = { :host => 'localhost' }
-
 # Initialize the rails application
 VendorLight::Application.initialize!
